2fbd4852f4a830457f2cd9e17c2bd1baa651f46b
Anna Waleska <anna.rigueira@zup.com.br>
2022-08-30 11:44:23 -0300

    Primeiro Commit - Spring Initializ

---
+++ src/main/java/com/elosete/annafreitas/model/Planetas.java
@@ -0,0 +1,8 @@
+public class Planetas {
+    // quantidade de planetas
+    // área permitida
+    // posicao da sonda no planeta
+    // colisao das sondas
+    // espaço disponivel em cada planeta
+
+}
---
+++ src/main/java/com/elosete/annafreitas/model/Sondas.java
@@ -0,0 +1,6 @@
+public class Sondas {
+    //contador de sondas já atribuidas
+    //posição final da sonda no planeta
+    //posição inicial da sonda quando colocada
+    //
+}
