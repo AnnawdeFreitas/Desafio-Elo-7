7f1c103ed684ccebd000bb5789c2bb955daad683
Anna Waleska <anna.rigueira@zup.com.br>
2022-08-30 13:44:29 -0300

    02 - Implementando algumas classes

---
+++ src/main/java/com/elosete/annafreitas/model/Sentido.java
@@ -0,0 +1,5 @@
+public class Sentido {
+    private Posicao posicao;
+    private Comandos comandos;
+
+}
--- src/main/java/com/elosete/annafreitas/model/Sondas.java
+++ src/main/java/com/elosete/annafreitas/model/Sondas.java
@@ -1,6 +1,40 @@
 public class Sondas {
-    //contador de sondas já atribuidas
-    //posição final da sonda no planeta
-    //posição inicial da sonda quando colocada
-    //
+    private Planetas planeta;
+    private Posicao posicao;
+    private Comandos comandos;
+
+    public Sondas(Planetas planeta, Sentido sentido) {
+        this(planeta, sentido.getPosicao(), sentido.getMovimentos());
+    }
+
+    public Sondas(Planetas planeta, Posicao posicao, Comandos comandos) {
+        this.comandos = comandos;
+        this.planeta = planeta;
+        this.posicao = posicao;
+
+        this.planeta.pousar(posicao, this);
+
+    }
+
+    public Posicao andarParaFrente() {
+        posicao novaPosicao = comandos.andarParaFrente(posicao);
+        planeta.mudaPosicaoAtual(posicao, novaPosicao);
+        this.posicao = novaPosicao;
+        return posicao;
+    }
+
+    public Comandos virarAEsquerda() {
+        this.comandos = comandos.virarAEsquerda();
+        return this.comandos;
+    }
+
+    public Comandos virarADireita() {
+        this.comandos = comandos.virarADireita();
+        return this.comandos;
+    }
+
+    public Sentido getSentidoAtual() {
+        return new Sentido(posicao, comandos);
+    }
+
 }
