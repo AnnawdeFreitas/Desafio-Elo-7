4e8b73b0423d2a739298e50122749ae536f3c6a2
Anna Waleska <anna.rigueira@zup.com.br>
2022-09-01 08:04:13 -0300

    implementando config

---
+++ src/main/java/com/elosete/annafreitas/configuracoes/AreaPlanaltoConfig.java
@@ -0,0 +1,43 @@
+package com.elosete.annafreitas.configuracoes;
+
+import java.io.Serializable;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "areaPlanalto")
+public class AreaPlanaltoConfig {
+
+    private Limite limite = new Limite();
+
+    public Limite getLimite() {
+        return limite;
+    }
+
+    public void setLimite(Limite limite) {
+        this.limite = limite;
+    }
+
+    public static class Limite implements Serializable {
+        private Integer x;
+        private Integer y;
+
+        public Integer getX() {
+            return x;
+        }
+
+        public void setX(Integer x) {
+            this.x = x;
+        }
+
+        public Integer getY() {
+            return y;
+        }
+
+        public void setY(Integer y) {
+            this.y = y;
+        }
+    }
+
+}
---
+++ src/main/java/com/elosete/annafreitas/configuracoes/ExplorandoConfig.java
@@ -0,0 +1,22 @@
+package com.elosete.annafreitas.configuracoes;
+
+import com.elosete.annafreitas.configuracoes.AreaPlanaltoConfig.Limite;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.elosete.annafreitas.componente.ExplorandoBuild;
+import com.elosete.annafreitas.modelo.EstacaoEspacial;
+
+@Configuration
+public class ExplorandoConfig {
+
+    @Bean
+    @Autowired
+    public EstacaoEspacial pegarEstacaoEspacial(AreaPlanaltoConfig areaPlanaltoConfig, ExplorandoBuild explorandoBuild) {
+         final Limite limite = areaPlanaltoConfig.getLimite();
+        return explorandoBuild.comLimiteDaAreaPlanalto(limite.getX(), limite.getY()).comPlaneta().comEstacaoEspacial()
+                .build();
+    }
+
+}
